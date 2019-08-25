9f69646a2d511a323cd6aa31f3e2e83ac10e7271
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-08-25 20:34:41 +0300

    Add files via upload

---
+++ TargilForTest250819/ValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TargilForTest250819
+{
+    class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (System.Convert.ToInt32(value) < 90)
+            { 
+                return "black";
+            }
+            return "pink";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TargilForTest250819/ViewModelOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargilForTest250819
+{
+    class ViewModelOrder
+    {
+
+        List<Order> orders = new List<Order>();
+        public ViewModelOrder()
+        {
+            orders.Add(new Order {ID = 1, FirstName = "e", LastName= "z", Age = 54, Email ="123", IsMember = true, OrderValue = 1.3, OrderStatus= MyEnum.Recived });
+        }           
+    }
+    
+    public enum MyEnum {New = 1, Shipped = 2, Processing = 3, Recived = 4, None = 5 }
+    class Order
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool IsMember { get; set; }
+        public double OrderValue { get; set; }
+        public MyEnum OrderStatus { get; set; }
+    }
+}
+
+
